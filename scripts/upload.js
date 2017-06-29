c82491f64a8b9e545363429e829f4c489d764163
aagman123 <learneach@gmail.com>
2017-06-29 07:57:50 -0700

    Add files via upload

---
+++ scripts/upload.js
@@ -0,0 +1,21 @@
+﻿function previewFile() {
+
+    var preview = document.querySelector('#source_image'); //selects the query named img
+    var file = document.querySelector('input[type=file]').files[0]; //sames as here
+    var reader = new FileReader();
+    //var orignialpreview = document.querySelector('img').files[1];
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+        orignialpreview = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+
+    }
+    else {
+        preview.src = "";
+        //orignialpreview.src = "";
+    }
+}
